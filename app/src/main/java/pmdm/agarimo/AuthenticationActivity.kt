f92d20addc368a6c19132e795498f8b02b954f97
joseangelsamperevazquez <jsamperevazquez@danielcastelao.org>
2022-01-19 18:32:43 +0100

    Limpieza código

--- app/src/main/java/pmdm/agarimo/AuthenticationActivity.kt
+++ app/src/main/java/pmdm/agarimo/AuthenticationActivity.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_auth.*
 import kotlin.properties.Delegates
