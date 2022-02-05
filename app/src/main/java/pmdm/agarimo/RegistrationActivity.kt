c50291b2629dbe52c5886664809b649e0b7dd057
joseangelsamperevazquez <jsamperevazquez@danielcastelao.org>
2022-02-05 15:27:15 +0100

    Añadida Activity para registro de clientes en FireBase

---
+++ app/src/main/java/pmdm/agarimo/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package pmdm.agarimo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
