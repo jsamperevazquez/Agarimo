d76d777557b4c857076d3d7f4709c44b1665a00b
joseangelsamperevazquez <jsamperevazquez@danielcastelao.org>
2022-02-11 13:25:39 +0100

    Creada data class para entidad Cliente

---
+++ app/src/main/java/pmdm/agarimo/entity/Client.kt
@@ -0,0 +1,8 @@
+package pmdm.agarimo.entity
+
+data class Client(
+    var name: String? = null,
+    var lastName: String? = null,
+    var email: String? = null,
+    var password: String? = null
+)
